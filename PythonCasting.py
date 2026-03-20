0e6bca17b530bbeffaee27e1133c6655940f9412
Urooj Fatima | EE Student <uf946345@gmail.com>
2026-03-21 00:20:17 +0500

    Add files via upload

---
+++ PythonCasting.py
@@ -0,0 +1,28 @@
+x = 1
+a = float(x)
+b = str(x)
+print(a)
+print(b)
+print(type(a))
+print(type(b))
+#python strings
+print("Hello")
+print('Hello')
+y = "Hello, world"
+print(y)
+z = """Hi my name is urooj
+I am nust student doing 
+EE"""
+print(z)
+#Strings are arrays
+x = "Hi,how are you"
+print(x[1])
+#Looping in string
+x = "Banana"
+for x in "Banana":
+    print(x)
+i = "My name is, Urooj"
+for i in "My name is, Urooj":
+    print(i)
+    #To get lenght
+    print(len(i))
