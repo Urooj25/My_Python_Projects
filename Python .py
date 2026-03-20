41697f068bf536dc62b2e61b1810b0619e03067a
Urooj Fatima | EE Student <uf946345@gmail.com>
2026-03-20 23:51:29 +0500

    Add files via upload

---
+++ Python .py
@@ -0,0 +1,14 @@
+#to import any random number
+import random
+print(random.randrange(1,9))
+#Task 2
+import random
+x = (1,5)
+print(random.randrange(*x))
+# way 2
+import random 
+start = 1
+stop = 10
+step = 3
+print(random.randrange(start,stop,step))#sirf 1 koi random print hu gha
+#Code Challenge
